0eda9ec4a5d2d462183dc5ccb452963016153c69
StyledStrike <lameulucas@gmail.com>
2023-11-22 15:23:41 -0300

    Tweak client-side entity var names

--- lua/entities/ent_musical_keyboard/cl_init.lua
+++ lua/entities/ent_musical_keyboard/cl_init.lua
@@ -1,18 +1,15 @@
 include( "shared.lua" )
 
-local RealTime = RealTime
-local Vector = Vector
-
 local blackKeys = {
     [1] = true, [3] = true, [6] = true, [8] = true, [10] = true
 }
 
 local keyColors = {
-    Color( 255, 148, 77 ),
-    Color( 171, 0, 197 )
+    default = Color( 255, 148, 77 ),
+    automated = Color( 171, 0, 197 )
 }
 
-local offsetKeys = {
+local keyOffsets = {
     [2] = 0.2,
     [4] = 0.4,
     [5] = -0.1,
@@ -22,18 +19,19 @@ local offsetKeys = {
 }
 
 local Remap = math.Remap
+local RealTime = RealTime
 
 function ENT:Initialize()
     self.drawNotes = {}
 end
 
-function ENT:EmitNote( note, velocity, level, instrument, automated )
-    local instr = MKeyboard.instruments[instrument]
+function ENT:EmitNote( note, velocity, instrument, automated )
+    local data = MKeyboard.instruments[instrument]
 
-    if not instr then return end
-    if note < instr.firstNote or note > instr.lastNote then return end
+    if not data then return end
+    if note < data.firstNote or note > data.lastNote then return end
 
-    sound.Play( string.format( instr.path, note ), self:GetPos(), level, 100, velocity / 127 )
+    sound.Play( string.format( data.path, note ), self:GetPos(), 80, 100, velocity / 127 )
 
     local idx = note % 12
     local len = 8
@@ -48,8 +46,8 @@ function ENT:EmitNote( note, velocity, level, instrument, automated )
         x = -0.6
     end
 
-    if offsetKeys[idx] then
-        x = x + offsetKeys[idx]
+    if keyOffsets[idx] then
+        x = x + keyOffsets[idx]
     end
 
     self.drawNotes[note] = {
@@ -57,7 +55,7 @@ function ENT:EmitNote( note, velocity, level, instrument, automated )
         t = RealTime() + 0.2,
         min = Vector( x, -1.5, -1 ),
         max = Vector( x + width, len, height ),
-        colorIndex = automated and 2 or 1
+        color = keyColors[automated and "automated" or "default"]
     }
 end
 
@@ -73,13 +71,13 @@ function ENT:Draw()
         if t > p.t then
             self.drawNotes[note] = nil
         else
-            local clr = keyColors[p.colorIndex]
+            local color = p.color
             local alpha = 255 * ( ( p.t - t ) / 0.2 )
 
             render.DrawBox(
                 self:LocalToWorld( Vector( -p.x, 0, 0 ) ),
                 ang, p.min, p.max,
-                Color( clr.r, clr.g, clr.b, alpha )
+                Color( color.r, color.g, color.b, alpha )
             )
         end
     end
